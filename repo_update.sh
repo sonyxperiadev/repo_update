ae8253028bf8dab85e506bd440478dd5d2a19bad
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-01 08:21:47 +0200

    add initial O cherry-pick list
    for easy sync and track we add the commits needed by the project
    after the repo sync.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

---
+++ repo_update.sh
@@ -0,0 +1,22 @@
+cd .repo/local_manifests
+git pull
+cd ../..
+
+repo sync -j40
+
+cd hardware/qcom/gps
+git fetch http://android.googlesource.com/platform/hardware/qcom/gps refs/changes/37/464137/1 && git cherry-pick FETCH_HEAD
+cd ../audio
+git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/91/294291/1 && git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/86/333386/1 && git cherry-pick FETCH_HEAD
+cd ../media
+git fetch http://android.googlesource.com/platform/hardware/qcom/media refs/changes/39/422439/2 && git cherry-pick FETCH_HEAD
+cd ../display
+git fetch http://android.googlesource.com/platform/hardware/qcom/display refs/changes/35/437235/1 && git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/hardware/qcom/display refs/changes/36/437236/1 && git cherry-pick FETCH_HEAD
+cd ../../../system/core
+git fetch http://android.googlesource.com/platform/system/core refs/changes/37/469437/1 && git cherry-pick FETCH_HEAD
+cd ../../frameworks/av
+git fetch http://android.googlesource.com/platform/frameworks/av refs/changes/92/384692/2 && git cherry-pick FETCH_HEAD
+cd ../../
+
