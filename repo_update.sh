4ef4dbb3bc777f7974dc08b7bb1789f3c1457462
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-29 09:36:43 +0200

    add the patches for odm partition support
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- repo_update.sh
+++ repo_update.sh
@@ -8,13 +8,15 @@ cd bionic
 git fetch http://android.googlesource.com/platform/bionic refs/changes/53/363153/1 && git cherry-pick FETCH_HEAD
 git fetch http://android.googlesource.com/platform/bionic refs/changes/92/368092/2 && git cherry-pick FETCH_HEAD
 git fetch http://android.googlesource.com/platform/bionic refs/changes/14/265214/21 && git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/bionic refs/changes/90/497890/1 && git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/bionic refs/changes/91/497891/2 && git cherry-pick FETCH_HEAD
 cd ../build/soong
 git fetch http://android.googlesource.com/platform/build/soong refs/changes/54/266354/28 && git cherry-pick FETCH_HEAD
 git fetch http://android.googlesource.com/platform/build/soong refs/changes/93/266393/16 && git cherry-pick FETCH_HEAD
 git fetch http://android.googlesource.com/platform/build/soong refs/changes/13/365413/2 && git cherry-pick FETCH_HEAD
 git fetch http://android.googlesource.com/platform/build/soong refs/changes/12/367312/2 && git cherry-pick FETCH_HEAD
 git fetch http://android.googlesource.com/platform/build/soong refs/changes/32/367332/3 && git cherry-pick FETCH_HEAD
-git fetch https://android.googlesource.com/platform/build/soong refs/changes/92/367392/2 && git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/build/soong refs/changes/92/367392/2 && git cherry-pick FETCH_HEAD
 cd ../../hardware/qcom/gps
 git fetch http://android.googlesource.com/platform/hardware/qcom/gps refs/changes/37/464137/1 && git cherry-pick FETCH_HEAD
 cd ../audio
@@ -29,6 +31,7 @@ cd ../bt
 git fetch http://android.googlesource.com/platform/hardware/qcom/bt refs/changes/17/478117/1 && git cherry-pick FETCH_HEAD
 cd ../../../system/core
 git fetch http://android.googlesource.com/platform/system/core refs/changes/37/469437/1 && git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/system/core refs/changes/92/497892/2 && git cherry-pick FETCH_HEAD
 cd ../../frameworks/av
 git fetch http://android.googlesource.com/platform/frameworks/av refs/changes/92/384692/2 && git cherry-pick FETCH_HEAD
 cd ../../packages/inputmethods/LatinIME
