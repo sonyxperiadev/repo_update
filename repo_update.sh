c805b283ca039223f728c7b02cf2554bfb806bfa
Alin Jerpelea <alin.jerpelea@sony.com>
2017-11-14 15:19:34 +0100

    Allow firmware loading from ODM partition
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- repo_update.sh
+++ repo_update.sh
@@ -92,6 +92,7 @@ pushd $ANDROOT/system/core
 LINK=$HTTP && LINK+="://android.googlesource.com/platform/system/core"
 git fetch $LINK refs/changes/37/469437/1 && git cherry-pick FETCH_HEAD
 git fetch $LINK refs/changes/92/497892/2 && git cherry-pick FETCH_HEAD
+git fetch $LINK refs/changes/75/537175/1 && git cherry-pick FETCH_HEAD
 popd
 
 pushd $ANDROOT/frameworks/av
