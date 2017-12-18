e35a5f42fa3d000eccecccc89f1a7ed720e80102
Pavel Dubrova <pashadubrova@gmail.com>
2017-12-19 01:36:25 +0200

    Update: post_proc for msm8952 path
    For some reason this patch can not be updated, so a new patch
    was sent to the Android review gerrit in the appropriate
    branch: oreo-m2-release
    
    Old patch:
    https://android-review.googlesource.com/#/c/platform/hardware/qcom/audio/+/333386/
    New patch:
    https://android-review.googlesource.com/#/c/platform/hardware/qcom/audio/+/573163/
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- repo_update.sh
+++ repo_update.sh
@@ -42,7 +42,7 @@ popd
 pushd $ANDROOT/hardware/qcom/audio
 LINK=$HTTP && LINK+="://android.googlesource.com/platform/hardware/qcom/audio"
 git fetch $LINK refs/changes/91/294291/1 && git cherry-pick FETCH_HEAD
-git fetch $LINK refs/changes/86/333386/1 && git cherry-pick FETCH_HEAD
+git fetch $LINK refs/changes/63/573163/1 && git cherry-pick FETCH_HEAD
 git fetch $LINK refs/changes/56/535256/1 && git cherry-pick FETCH_HEAD
 popd
 
