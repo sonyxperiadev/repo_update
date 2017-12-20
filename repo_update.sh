fa1a88f2e34483e7a17a0ea9d2b3023187ad8f3e
Pavel Dubrova <pashadubrova@gmail.com>
2017-12-20 08:44:40 +0200

    Update: system/core patch
    This patch is identical but the domain name in the
    author's email has been changed, so they have been updated.
    
    Old patch:
    https://android-review.googlesource.com/#/c/platform/system/core/+/497892/
    New patch:
    https://android-review.googlesource.com/#/c/platform/system/core/+/553221/
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- repo_update.sh
+++ repo_update.sh
@@ -64,7 +64,7 @@ popd
 
 pushd $ANDROOT/system/core
 LINK=$HTTP && LINK+="://android.googlesource.com/platform/system/core"
-git fetch $LINK refs/changes/92/497892/2 && git cherry-pick FETCH_HEAD
+git fetch $LINK refs/changes/21/553221/1 && git cherry-pick FETCH_HEAD
 popd
 
 pushd $ANDROOT/frameworks/av
