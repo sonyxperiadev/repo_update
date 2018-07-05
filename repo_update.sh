6a304b40f3650e89862458204945a81e03aa5b1f
Alin Jerpelea <alin.jerpelea@sony.com>
2018-07-05 08:43:14 +0200

    msm8996: fix build
    add hardware/qcom/media commits:
    
    https://android-review.googlesource.com/c/platform/hardware/qcom/media/+/713242
    https://android-review.googlesource.com/c/platform/hardware/qcom/media/+/713243
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- repo_update.sh
+++ repo_update.sh
@@ -62,6 +62,8 @@ git fetch $LINK refs/changes/55/522855/1 && git cherry-pick FETCH_HEAD
 git fetch $LINK refs/changes/15/708815/1 && git cherry-pick FETCH_HEAD
 git fetch $LINK refs/changes/16/708816/1 && git cherry-pick FETCH_HEAD
 git fetch $LINK refs/changes/17/708817/1 && git cherry-pick FETCH_HEAD
+git fetch $LINK refs/changes/42/713242/1 && git cherry-pick FETCH_HEAD
+git fetch $LINK refs/changes/43/713243/1 && git cherry-pick FETCH_HEAD
 popd
 
 pushd $ANDROOT/hardware/qcom/display
