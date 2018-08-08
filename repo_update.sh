d3d441f04c79c23f1c86097c4a621d40d5183608
pablomh <pablomh@gmail.com>
2018-08-08 15:01:43 +0200

    Remove unneeded bionic patches
    They're already included in Pie.

--- repo_update.sh
+++ repo_update.sh
@@ -24,12 +24,6 @@ if [ "$SKIP_SYNC" != "TRUE" ]; then
     repo sync -j8 --current-branch --no-tags
 fi
 
-pushd $ANDROOT/bionic
-LINK=$HTTP && LINK+="://android.googlesource.com/platform/bionic"
-git fetch $LINK refs/changes/59/555059/1 && git cherry-pick FETCH_HEAD
-git fetch $LINK refs/changes/22/553222/1 && git cherry-pick FETCH_HEAD
-popd
-
 pushd $ANDROOT/frameworks/base
 LINK=$HTTP && LINK+="://android.googlesource.com/platform/frameworks/base"
 git fetch $LINK refs/changes/19/642919/5 && git cherry-pick FETCH_HEAD
