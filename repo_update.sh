696ba073e05aa4ac9ff8abf7492fb25ed6c7253f
MarijnS95 <marijns95@gmail.com>
2019-09-04 15:08:53 +0200

    [Android-10] hardware: qcom: audio: Add missing bracket.
    Someone forgot a bracket, making the code invalid.
    
    Signed-off-by: MarijnS95 <marijns95@gmail.com>

--- repo_update.sh
+++ repo_update.sh
@@ -104,6 +104,8 @@ apply_gerrit_cl_commit refs/changes/00/1112100/2 eeecf8a399080598e5290d3356b0ad5
 # hal: msm8916: Fix for vndk compilation errors
 # Change-Id: Iffd8a3c00a2a1ad063e10c0ebf3ce9e88e3edea0
 apply_gerrit_cl_commit refs/changes/14/777714/1 065ec9c4857fdd092d689a0526e0caeaaa6b1d72
+
+apply_local_patches
 popd
 
 pushd $ANDROOT/hardware/qcom/media
