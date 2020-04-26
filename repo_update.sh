220235f17bc70ff66d1216193021883419542fe2
ix5 <ix5@users.noreply.github.com>
2020-04-26 20:32:11 +0200

    fwb: Update hash for rebased commit
    git can still apply this patch, but using pre-rebased
    commits will shave off those few annoying seconds of
    "Performing inexact rename detection".

--- repo_update.sh
+++ repo_update.sh
@@ -125,7 +125,7 @@ pushd $ANDROOT/frameworks/base
 LINK=$HTTP && LINK+="://android.googlesource.com/platform/frameworks/base"
 # Fix bug Device that can't support adoptable storage cannot read the sdcard.
 # Change-Id: I7afe5078650fe646e79fced7456f90d4af8a449a
-apply_gerrit_cl_commit refs/changes/05/728605/1 b6f563436ca1b1496bf6026453e5b805c856f9e6
+apply_gerrit_cl_commit refs/changes/48/1295748/1 6ec651f12a9b67a9d2e41c2fe4d9a71c29d1cf34
 # SystemUI: Implement burn-in protection for status-bar/nav-bar items
 # Change-Id: I828dbd4029b4d3b1f2c86b682a03642e3f9aeeb9
 apply_gerrit_cl_commit refs/changes/40/824340/2 cf575e7f64a976918938e6ea3bc747011fb3b551
